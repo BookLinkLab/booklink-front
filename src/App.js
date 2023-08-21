84f4f19d865adc65f7bd3b57783901410396d342
maxigeist <geist.maximo@gmail.com>
2023-08-21 18:12:27 -0300

    feat:add login screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<Login />}></Route>
+                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
     )
