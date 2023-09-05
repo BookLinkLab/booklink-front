6d62d68a0996de42990550b7e9be60d4b85218c1
franciscocavallaro <francisco.cavallaro@ing.austral.edu.ar>
2023-09-05 19:45:28 -0300

    PR Modifications done, still tags and member components missing

--- src/components/HeaderForum/HeaderForum.jsx
+++ src/components/HeaderForum/HeaderForum.jsx
@@ -1,28 +1,27 @@
 import React from "react"
 import Button from "../Button"
+import "./styles.css"
 
 const HeaderForum = ({ title, description, image, owner, membersQuantity, tags }) => {
     return (
-        <div style={{ display: "flex", alignItems: "flex-end", justifyContent: "center" }}>
-            <div>
-                <img src={image} alt="header-forum" />
-            </div>
+        <div className="headerForum">
+            <img className="forumImage" src={image} alt="header-forum" />
             <div style={{ width: 581, marginLeft: 32, marginRight: 40 }}>
                 <h5 className="bold">{title}</h5>
                 <text>{description}</text>
             </div>
-            <div>
-                {owner ? (
-                    <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
-                        <Button variant="outlined" size="small">
-                            Editar
-                        </Button>
-                        <Button size="small">Eliminar</Button>
-                    </div>
-                ) : (
+            {owner ? (
+                <div className="headerForumButtonContainer">
+                    <Button variant="outlined" size="small">
+                        Editar
+                    </Button>
+                    <Button size="small">Eliminar</Button>
+                </div>
+            ) : (
+                <div className="headerForumButtonContainer">
                     <Button size="small">Abandonar</Button>
-                )}
-            </div>
+                </div>
+            )}
             {/* tags and members missing*/}
         </div>
     )
--- src/screens/forum/forum.jsx
+++ src/screens/forum/forum.jsx
@@ -4,24 +4,38 @@ import { getForum } from "../../service/apis"
 import { useCurrentUser } from "../../hooks/useCurrentUser"
 import { useParams } from "react-router-dom"
 
-export const Forum = async () => {
-    const { id } = useParams()
+const Forum = () => {
+    // const { id } = useParams();
+    //
+    // const forum = await getForum(id, useCurrentUser().token);
+    //
+    // const isOwner = useCurrentUser().id === forum.ownerId;
 
-    const forum = await getForum(id, useCurrentUser().token)
-
-    const isOwner = useCurrentUser().id === forum.ownerId
-
-    const [loading, setLoading] = useState(false)
+    const mock = {
+        image: "https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg",
+        title: "el pepe",
+        description: "el pepe hbdsbvbdc",
+        membersQuantity: 48,
+        tags: ["Tag 1", "Tag 2", "Tag 3"],
+    }
 
     return (
         <>
+            {/*<HeaderForum*/}
+            {/*    title={forum.title}*/}
+            {/*    description={forum.description}*/}
+            {/*    image={forum.image}*/}
+            {/*    owner={isOwner}*/}
+            {/*    membersQuantity={forum.membersQuantity}*/}
+            {/*    tags={forum.tags.map((tag) => tag.name)}*/}
+            {/*/>*/}
             <HeaderForum
-                title={forum.title}
-                description={forum.description}
-                image={forum.image}
-                owner={isOwner}
-                membersQuantity={forum.membersQuantity}
-                tags={forum.tags.map((tag) => tag.name)}
+                title={mock.title}
+                description={mock.description}
+                image={mock.image}
+                owner={false}
+                membersQuantity={mock.membersQuantity}
+                tags={mock.tags.map((tag) => tag.name)}
             />
         </>
     )
