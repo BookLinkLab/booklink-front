5be080a84eb838234e821c32cf01abdf963f3ddd
maxigeist <geist.maximo@gmail.com>
2023-09-14 07:47:29 -0300

    feat: add modal component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -15,22 +15,29 @@ const Modal = ({
     const [modal, setShowModal] = useState(showModal)
     if (modal) {
         return (
-            <div className="modal-main-div">
-                <div className="remove-div" onClick={() => setShowModal(false)}>
-                    <Remove width={18} height={18} />
-                </div>
-                <div className="text-modal-div">
-                    <h5 className="text-modal">{title}</h5>
-                    <p className="body2">{subtitle}</p>
-                </div>
-                <div className="buttons-modal-div">
-                    <div className="button-width">
-                        <Button variant="outlined" size="medium" onClick={firstButtonAction}>
+            <div className={"modal-container"}>
+                <div className="modal-main-div">
+                    <div className="remove-div" onClick={() => setShowModal(false)}>
+                        <Remove width={18} height={18} />
+                    </div>
+                    <div className="text-modal-div">
+                        <h5 className="text-modal">{title}</h5>
+                        <p className="body2">{subtitle}</p>
+                    </div>
+                    <div className="buttons-modal-div">
+                        <Button
+                            variant="outlined"
+                            size="medium"
+                            className={"button-width"}
+                            onClick={firstButtonAction}
+                        >
                             {firstButtonText}
                         </Button>
-                    </div>
-                    <div className="button-width">
-                        <Button size="medium" onClick={secondButtonAction}>
+                        <Button
+                            size="medium"
+                            className={"button-width"}
+                            onClick={secondButtonAction}
+                        >
                             {secondButtonText}
                         </Button>
                     </div>
