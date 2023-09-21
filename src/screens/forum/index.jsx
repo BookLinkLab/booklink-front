9f186f948a73ace9a7661468a31bb784d5485757
franciscocavallaro <francisco.cavallaro@ing.austral.edu.ar>
2023-09-21 13:05:48 -0300

    fix: integration forum 3.0

--- src/screens/forum/index.jsx
+++ src/screens/forum/index.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react"
 import HeaderForum from "../../components/HeaderForum"
-import { getForum } from "../../service/apis"
 import { useCurrentUser } from "../../hooks/useCurrentUser"
 import { useNavigate, useParams } from "react-router-dom"
 import withToast from "../../hoc/withToast"
 import Loader from "../../components/Loader"
+import { getForum, leaveForum } from "../../service/apis"
 
-const Forum = ({ showExternalToast }) => {
+const Forum = ({ showToast, showExternalToast }) => {
     const { forumId } = useParams()
     const { token, id } = useCurrentUser()
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
     const [forum, setForum] = useState({})
+    // const { id } = useParams();
+    //
+    // const forum = await getForum(id, useCurrentUser().token);
+    //
+    // const isOwner = useCurrentUser().id === forum.ownerId;
 
     useEffect(() => {
         setLoading(true)
@@ -27,6 +32,21 @@ const Forum = ({ showExternalToast }) => {
         } else {
             showExternalToast("Error al cargar el foro")
             navigate("/home")
+        }
+    }
+
+    const clickLeaveForum = async () => {
+        setLoading(true)
+        try {
+            const resp = await leaveForum(token, forum.id)
+            if (resp.status === 200) {
+                showToast(resp.body, "success")
+                navigate("/home")
+            } else {
+                showToast(resp.body, "error")
+            }
+        } finally {
+            setLoading(false)
         }
     }
 
