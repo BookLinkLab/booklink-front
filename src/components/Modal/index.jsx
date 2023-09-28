bc5e6eda2f3cb88ddcea0cf4b53d8c717bc86b5e
franciscocavallaro <francisco.cavallaro@ing.austral.edu.ar>
2023-09-27 23:55:16 -0300

    hotfix: demo2 modals

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -10,41 +10,34 @@ const Modal = ({
     secondButtonText,
     firstButtonAction,
     secondButtonAction,
-    showModal,
+    handleOnClose,
 }) => {
-    const [modal, setShowModal] = useState(showModal)
-    if (modal) {
-        return (
-            <div className={"modal-container"}>
-                <div className="modal-main-div">
-                    <div className="remove-div" onClick={() => setShowModal(false)}>
-                        <Remove width={18} height={18} />
-                    </div>
-                    <div className="text-modal-div">
-                        <h5 className="text-modal">{title}</h5>
-                        <p className="body2">{subtitle}</p>
-                    </div>
-                    <div className="buttons-modal-div">
-                        <Button
-                            variant="outlined"
-                            size="medium"
-                            className={"button-width"}
-                            onClick={firstButtonAction}
-                        >
-                            {firstButtonText}
-                        </Button>
-                        <Button
-                            size="medium"
-                            className={"button-width"}
-                            onClick={secondButtonAction}
-                        >
-                            {secondButtonText}
-                        </Button>
-                    </div>
+    return (
+        <div className={"modal-container"}>
+            <div className="modal-main-div">
+                <div className="remove-div" onClick={handleOnClose}>
+                    <Remove width={18} height={18} />
+                </div>
+                <div className="text-modal-div">
+                    <h5 className="text-modal">{title}</h5>
+                    <p className="body2">{subtitle}</p>
+                </div>
+                <div className="buttons-modal-div">
+                    <Button
+                        variant="outlined"
+                        size="medium"
+                        className={"button-width"}
+                        onClick={firstButtonAction}
+                    >
+                        {firstButtonText}
+                    </Button>
+                    <Button size="medium" className={"button-width"} onClick={secondButtonAction}>
+                        {secondButtonText}
+                    </Button>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Modal
