8ff03aa9a976ef32300b67dc165fcceb0c925e54
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2023-10-19 15:17:21 -0300

    fix: forum logic comments

--- src/screens/forum/index.jsx
+++ src/screens/forum/index.jsx
@@ -83,28 +83,31 @@ const Forum = ({ showToast }) => {
                 isMember={forum.searcherIsMember}
                 setForumData={setForum}
             />
-            <div className="addPostContainer">
-                <AddPost
-                    textFieldPlaceholder={"Comparte tus ideas"}
-                    onClick={handleAddPost}
-                    buttonText={"Crear publicación"}
-                    onSubmit={(comment) => handleAddPost(comment).then(getPostsData)}
-                />
-            </div>
-            <div className="postsContainer">
-                {posts.map((post) => (
-                    <Comment
-                        commentText={post.content}
-                        username={post.user.username}
-                        commentDate={post.date}
-                        isPost={true}
-                        owner={post.user.id == id}
-                        id={post.id}
-                        refresh={getPostsData}
-                        key={post.id}
-                    />
-                ))}
-            </div>
+            {id === forum.searcherIsMember && (
+                <>
+                    <div className="addPostContainer">
+                        <AddPost
+                            textFieldPlaceholder={"Comparte tus ideas"}
+                            onClick={handleAddPost}
+                            buttonText={"Crear publicacion"}
+                            onSubmit={(comment) => handleAddPost(comment)}
+                        />
+                    </div>
+                    <div className="postsContainer">
+                        posts.map((post) => (
+                        <Comment
+                            commentText={post.content}
+                            username={post.user.username}
+                            commentDate={post.date}
+                            isPost={true}
+                            owner={post.user.id === id}
+                            id={post.id}
+                            refresh={getPostsData}
+                            key={post.id}
+                        />
+                    </div>
+                </>
+            )}
         </>
     )
 }
