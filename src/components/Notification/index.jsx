1cc483a3d2c89ce88e8d5e18a19507f8fa6e6c31
franciscocavallaro <francisco.cavallaro@ing.austral.edu.ar>
2023-11-09 09:47:59 -0300

    chore: conflicts resolved

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -4,14 +4,8 @@ import "./styles.css"
 const Notification = ({ forumImg, content, forumName, authorName, isSeen = true, id, onClick }) => {
     const seenChecker = isSeen ? "notificationContainer seen" : "notificationContainer"
 
-    const handleClick = () => {
-        if (!isSeen) {
-            console.log("redirection")
-        }
-    }
-
     return (
-        <div className={seenChecker} onClick={handleClick}>
+        <div className={seenChecker} onClick={onClick}>
             <img className="forumImage" src={forumImg} alt="notification" />
             <div className="notificationText">
                 <div>
